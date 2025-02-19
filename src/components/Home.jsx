79ff90161342307e2483d2ecc9d5e84fe279c344
wasiiff <wasifgee0012@gmail.com>
2025-02-20 01:35:53 +0500

    last

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import wasif from "../assets/image.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
 const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b 
-    from-black via-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div
-        className="max-w-screen-lg mx-auto mr-16 flex items-center justify-center h-full px-4 md:flex-row"
-      >
+      <div className="max-w-screen-lg mx-auto mr-16 flex items-center justify-center h-full px-4 md:flex-row">
         <div className="flex mt-16 flex-col justify-center h-full">
-          <h2
-            className="text-4xl sm:text-6xl font-bold
-                text-white "
-          >
+          <h2 className="text-4xl sm:text-6xl font-bold text-white">
             I' am a Frontend Developer
           </h2>
           <p className="text-gray-400 py-4">
@@ -25,25 +20,12 @@ const Home = () => {
             experiences, while Tailwind CSS enables me to design sleek,
             responsive layouts with efficiency and precision.
           </p>
-
-          {/* <div>
-            <button
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center
-                    rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-              </span>
-            </button>
-          </div> */}
         </div>
         <div className="md:mt-8">
           <img
             src={wasif}
             alt="my profile"
-            className="  rounded-2xl md:mt-11 
-                mx-auto md:w-2/3 grid-cols-4 shadow-blue-700"
+            className="rounded-2xl md:mt-11 mx-auto sm:w-3/4 sm:h-3/4 md:w-2/3 md:h-2/3 shadow-blue-700"
           />
         </div>
       </div>
